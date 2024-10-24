c8cf50c02d3f4e9fac1edb93f58a90ec5d3b155a
daviH98 <h.davi@pucpr.edu.br>
2024-10-23 21:33:00 -0300

    criei classe estoque
    alteração da classe estoque

--- Estoque.java
+++ Estoque.java
@@ -1,2 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Estoque {
+    private List<Produto> produtos;
+
+public Estoque() {
+    produtos = new ArrayList<>();
+
+    public void adicionarProduto (Produto produtos){
+        produtos.add(produtos);
+    }
+
+    public void removerProduto (Produto produtos){
+        produtos.remove(produtos);
+    }
+
+    public List<Produto> listarProdutos () {
+        return produtos
+    }
+}
 }
