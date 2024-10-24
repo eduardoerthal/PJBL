8c5155d5f5f1a2594c3ec209659d7b80e7192c09
eduardoerthal <eduardoerthaloliveira@gmail.com>
2024-10-23 21:05:57 -0300

    adicionei classe produto

---
+++ Produto.java
@@ -0,0 +1,13 @@
+public class Produto{
+    private String nome;
+    private double preco;
+    private String tamanho;
+    private String cor;
+
+    public Produto(String nome, double preco, String tamanho, String cor){
+        this.nome = nome;
+        this.preco = preco;
+        this.tamanho = tamanho;
+        this.cor = cor;
+    }
+}
